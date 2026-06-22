7a04800dfd71a358be8c916d8915f6b7b5e4e1d9
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-06-22 14:18:26 -0400

    Squashed commit of the following:
    commit 38773b94cb2d672bea47170cb301fff9c629d6b9
    Author: AquaFly1 <louis.emery@hotmail.com>
    Date:   Mon Jun 22 14:16:18 2026 -0400
    
        lucidity system

--- platformer-jam/Scripts/Globals/event_manager.gd
+++ platformer-jam/Scripts/Globals/event_manager.gd
@@ -1,2 +1,12 @@
 class_name EventManager
 extends Node
+
+signal lucidity_changed(value)
+
+func set_lucidity(value):
+	Game.lucidity = value
+	lucidity_changed.emit(Game.lucidity)
+
+func change_lucidity(value):
+	Game.lucidity += value
+	lucidity_changed.emit(Game.lucidity)
--- platformer-jam/Scripts/Globals/game_manager.gd
+++ platformer-jam/Scripts/Globals/game_manager.gd
@@ -3,3 +3,13 @@ extends Node
 
 @onready var audio_manager: AudioManager = $AudioManager
 @onready var event_manager: EventManager = $EventManager
+
+var lucidity: float = 0
+
+func _ready() -> void:
+	lucid_test()
+
+func lucid_test():
+	event_manager.change_lucidity(1)
+	await get_tree().create_timer(0.01).timeout
+	lucid_test()
