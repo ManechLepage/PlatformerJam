9275c03bd2a65457b1a8b61d292aa0b026564f0f
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-06-22 06:56:21 -0400

    Basics

---
+++ platformer-jam/Scripts/Globals/event_manager.gd
@@ -0,0 +1,2 @@
+class_name EventManager
+extends Node
---
+++ platformer-jam/Scripts/Globals/game_manager.gd
@@ -0,0 +1,5 @@
+class_name Game_Manager
+extends Node
+
+@onready var audio_manager: AudioManager = $AudioManager
+@onready var event_manager: EventManager = $EventManager
