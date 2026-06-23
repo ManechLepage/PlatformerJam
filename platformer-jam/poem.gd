df9d98bc6e62069951bc6fa9a1506d7fec2dbc7e
eliradon <110346765+eliradon@users.noreply.github.com>
2026-06-23 12:28:58 -0400

    chameau
    poeme

--- platformer-jam/poem.gd
+++ platformer-jam/poem.gd
@@ -1,11 +1,35 @@
 extends Node2D
-
-
+@onready var Area: Area2D = $Area
+@export var e_key: Sprite2D
+@export var poem_text: RichTextLabel
+var has_been_found: bool = false
+var progress: float = 0.0
+var transparency: float = 0.0
+var activated: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Area.body_entered.connect(_on_body_entered)
+	Area.body_exited.connect(_on_body_exited)
+	poem_text.modulate.a = 0
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if activated:
+		progress += delta
+		poem_text.visible_characters = int(progress*20)
+		if poem_text.modulate.a < 1:
+			poem_text.modulate.a += delta*3
+	else: 
+		if poem_text.modulate.a > 0:
+			poem_text.modulate.a -= delta*3
+		
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		activated = true
+		
+func _on_body_exited(body):
+	if body.name == "Player":
+		activated = false
