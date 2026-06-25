edac43492af839e97dddc83ad85e18bfd6afea87
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-06-25 17:58:49 -0400

    ag

--- platformer-jam/Scripts/Objects/lever.gd
+++ platformer-jam/Scripts/Objects/lever.gd
@@ -15,4 +15,3 @@ func _on_body_exited(body: Node2D) -> void:
 func activate():
 	activated = true
 	on_activated.emit()
-	animation_player.play("Activate")
---
+++ platformer-jam/Scripts/Room/lever_anim1.gd
@@ -0,0 +1,5 @@
+extends AnimationPlayer
+
+
+func _on_lever_on_activated() -> void:
+	play("LeverAnim")
