ef0aa05e530c6232b07d603a0b0525c6603d2781
Ti-Die <ewm.hoover@gmail.com>
2026-06-26 17:42:19 -0400

    fixed waterfall

--- platformer-jam/Scripts/Objects/fallwater.gd
+++ platformer-jam/Scripts/Objects/fallwater.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 func on_lucidity_changed() -> void:
 	if ray_cast_2d.is_colliding():
 		var height: float = ray_cast_2d.get_collision_point().y
-		collision_shape_2d.scale.y = (height - global_position.y)/64
+		collision_shape_2d.scale.y = (height - global_position.y)
 		collision_shape_2d.position.y = (height-global_position.y)/2
 		$Sprite2D.region_rect = Rect2(0,0,64*3,height-global_position.y)
 		$Sprite2D.position.y = (height-global_position.y)/2
