be8c37f906d679cfd660014778d4c6e220947d0a
eliradon <110346765+eliradon@users.noreply.github.com>
2026-06-26 19:33:50 -0400

    mini update tower 1

--- platformer-jam/Scripts/Objects/platform.gd
+++ platformer-jam/Scripts/Objects/platform.gd
@@ -6,7 +6,9 @@ extends AnimatableBody2D
 @onready var hitbox: CollisionShape2D = find_child("CollisionShape2D")
 
 func _ready() -> void:
-	if is_platform and hitbox: hitbox.one_way_collision = true
+	if is_platform and hitbox:
+		hitbox.one_way_collision = true
+		hitbox.one_way_collision_margin = 5
 	Game.event_manager.lucidity_changed.connect(change_lucidity)
 	
 func change_lucidity(val):
