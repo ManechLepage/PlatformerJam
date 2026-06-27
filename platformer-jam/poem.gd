ad8c3a4f3349700a60b66cd68bb0b62d3308dab3
AquaFly1 <louis.emery@hotmail.com>
2026-06-27 18:12:42 -0400

    poem

--- platformer-jam/poem.gd
+++ platformer-jam/poem.gd
@@ -1,6 +1,8 @@
 class_name Poem
 extends Area2D
 @export var poem_text: RichTextLabel
+
+@onready var image: Sprite2D = $CanvasLayer/Image
 var has_been_found: bool = false
 @export var destroyable: bool = true
 var progress: float = 0.0
@@ -12,6 +14,7 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	area_exited.connect(_on_area_exited)
 	poem_text.modulate.a = 0
+	image.modulate.a = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,12 +23,15 @@ func _process(delta: float) -> void:
 		poem_text.visible_characters = int(progress*20)
 		if poem_text.modulate.a < 1:
 			poem_text.modulate.a += delta*3
+			image.modulate.a += delta*3
+			
 		if poem_text.visible_characters >= len(poem_text.text):
 			Game.event_manager.interactable_object = self
 
 	else: 
 		if poem_text.modulate.a > 0 and not collecting:
-			poem_text.modulate.a -= delta*3
+			poem_text.modulate.a += delta*3
+			image.modulate.a += delta*3
 
 		
 func _on_area_entered(body):
